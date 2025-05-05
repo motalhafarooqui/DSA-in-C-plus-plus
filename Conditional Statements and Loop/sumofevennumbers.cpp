2ae41ba053ee05f1c40dd87032f4624ad5a28456
Mohd. Talha Farooqui <mohdtalhafarooqui@gmail.com>
2025-05-05 10:46:31 +0530

    Update sumofevennumbers.cpp

--- Conditional Statements and Loop/sumofevennumbers.cpp
+++ Conditional Statements and Loop/sumofevennumbers.cpp
@@ -1,10 +1,12 @@
+//Code to find the sum of even numbers which are between 1 and n.
+
 #include <iostream>
 using namespace std;
 int main() {
     int a, evenSum, i;
     i = 2;
     evenSum = 0;
-    cout << "Enter the number of natural numbers till which you wamt to add even from 1 : " ;
+    cout << "Enter the number of natural numbers till which you want to add even from 1 : " ;
     cin >> a;
     /*for(int i=1; i<=a; i=i+2){
         sum = sum + i;
