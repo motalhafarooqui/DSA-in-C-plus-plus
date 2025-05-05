5baa1ba1714c88d578ad0ac4635b3f4ecd2a19cb
Mohd. Talha Farooqui <mohdtalhafarooqui@gmail.com>
2025-05-05 10:48:54 +0530

    Update sumofoddnumbers.cpp

--- Conditional Statements and Loop/sumofoddnumbers.cpp
+++ Conditional Statements and Loop/sumofoddnumbers.cpp
@@ -1,10 +1,12 @@
+//Code to find the sum of all the natural numbers between 1 and n that are odd.
+
 #include <iostream>
 using namespace std;
 int main() {
     int a, sum, i;
     i = 1;
     sum = 0;
-    cout << "Enter the number of natural numbers till which you wamt to add odd from 1 : " ;
+    cout << "Enter the number of natural numbers till which you want to add odd numbers from 1 : " ;
     cin >> a;
     /*for(int i=1; i<=a; i=i+2){
         sum = sum + i;
